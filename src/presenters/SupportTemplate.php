eb2a2ab6ac84d3c44c32dc6e7ce75f133a155a7f
Milos Havlicek <miloshavlicek@gmail.com>
2014-06-27 10:16:25 +0200

    + support

---
+++ src/presenters/SupportTemplate.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette,
+    App\Model;
+use App;
+use Nette\Application\UI\Form;
+
+/**
+ * Support presenter.
+ */
+class SupportPresenter extends BasePresenter
+{
+    
+    public function renderDefault() {
+        
+        $navbar = [];
+        $navbar[] = (object)['name' => 'Pomoc'];
+        $navbar[] = (object)['name' => 'Podpora systému'];
+        
+        $this->template->navbar = $navbar;
+        
+        $contact = (object)[
+                'name' => 'Miloš Havlíček',
+                'tel' => '+420 606 460 316',
+                'email' => 'miloshavlicek@gmail.com'
+            ];
+        
+        $this->template->contact = $contact;
+    }
+    
+}
