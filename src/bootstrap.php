21da7f7591ab7de3243ea3ee83f9886884f729f8
Milos Havlicek <miloshavlicek@gmail.com>
2014-09-05 13:35:43 +0200

    + kdyby translation (prepare), bootstrap moved from skeleton to vendor

---
+++ src/bootstrap.php
@@ -0,0 +1,46 @@
+<?php
+
+use Nette\Forms\Container;
+use Nextras\Forms\Controls;
+
+Container::extensionMethod('addOptionList', function (Container $container, $name, $label = NULL, array $items = NULL) {
+    return $container[$name] = new Controls\OptionList($label, $items);
+});
+Container::extensionMethod('addMultiOptionList', function (Container $container, $name, $label = NULL, array $items = NULL) {
+    return $container[$name] = new Controls\MultiOptionList($label, $items);
+});
+Container::extensionMethod('addDatePicker', function (Container $container, $name, $label = NULL) {
+    return $container[$name] = new Controls\DatePicker($label);
+});
+Container::extensionMethod('addDateTimePicker', function (Container $container, $name, $label = NULL) {
+    return $container[$name] = new Controls\DateTimePicker($label);
+});
+Container::extensionMethod('addTypeahead', function(Container $container, $name, $label = NULL, $callback = NULL) {
+    return $container[$name] = new Controls\Typeahead($label, $callback);
+});
+
+$configurator = new Nette\Configurator;
+
+$configurator->setDebugMode(['127.0.0.1']);
+$configurator->enableDebugger(DIR_ROOT . '/log');
+
+$configurator->setTempDirectory(DIR_ROOT . '/temp');
+
+$configurator->createRobotLoader()
+        ->addDirectory(DIR_VENDOR . '/cothema/')
+        ->addDirectory(DIR_ROOT . '/vendor-manual/')
+        ->register();
+
+$configurator->addConfig(__DIR__ . '/config/config.neon');
+$configurator->addConfig(DIR_ROOT . '/app/config/config.local.neon');
+
+/* Doctrine */
+Kdyby\Annotations\DI\AnnotationsExtension::register($configurator);
+Kdyby\Console\DI\ConsoleExtension::register($configurator);
+Kdyby\Events\DI\EventsExtension::register($configurator);
+Kdyby\Doctrine\DI\OrmExtension::register($configurator);
+/* end: Doctrine */
+
+$container = $configurator->createContainer();
+
+return $container;
