00fda4b13d63408266dd954bb4a3e7e339a03ef0
Milos Havlicek <miloshavlicek@gmail.com>
2015-02-18 09:04:18 +0100

    robotLoaderDirs as array

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,34 +4,43 @@ use Nette\Forms\Container;
 use Nextras\Forms\Controls;
 
 Container::extensionMethod('addOptionList', function (Container $container, $name, $label = NULL, array $items = NULL) {
-    return $container[$name] = new Controls\OptionList($label, $items);
+	return $container[$name] = new Controls\OptionList($label, $items);
 });
 Container::extensionMethod('addMultiOptionList', function (Container $container, $name, $label = NULL, array $items = NULL) {
-    return $container[$name] = new Controls\MultiOptionList($label, $items);
+	return $container[$name] = new Controls\MultiOptionList($label, $items);
 });
 Container::extensionMethod('addDatePicker', function (Container $container, $name, $label = NULL) {
-    return $container[$name] = new Controls\DatePicker($label);
+	return $container[$name] = new Controls\DatePicker($label);
 });
 Container::extensionMethod('addDateTimePicker', function (Container $container, $name, $label = NULL) {
-    return $container[$name] = new Controls\DateTimePicker($label);
+	return $container[$name] = new Controls\DateTimePicker($label);
 });
 Container::extensionMethod('addTypeahead', function(Container $container, $name, $label = NULL, $callback = NULL) {
-    return $container[$name] = new Controls\Typeahead($label, $callback);
+	return $container[$name] = new Controls\Typeahead($label, $callback);
 });
 
 $configurator = new Nette\Configurator;
 
-if(isset($debugIPs)) {
+if (isset($debugIPs)) {
 	$configurator->setDebugMode($debugIPs);
 }
 $configurator->enableDebugger(DIR_ROOT . '/log');
 
 $configurator->setTempDirectory(DIR_ROOT . '/temp');
 
-$configurator->createRobotLoader()
-        ->addDirectory(DIR_VENDOR . '/cothema/')
-        ->addDirectory(DIR_ROOT . '/vendor-manual/')
-        ->register();
+if (!isset($robotLoaderDirs)) {
+	$robotLoaderDirs = [];
+}
+$robotLoaderDirs[] = DIR_VENDOR . '/cothema/';
+$robotLoaderDirs[] = DIR_ROOT . '/vendor-manual/';
+
+$robotLoader = $configurator->createRobotLoader();
+
+foreach ($robotLoaderDirs as $robotLoaderDirsOne) {
+	$robotLoader->addDirectory($robotLoaderDirsOne);
+}
+
+$robotLoader->register();
 
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(DIR_ROOT . '/app/config/config.local.neon');
