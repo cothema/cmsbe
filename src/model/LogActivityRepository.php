56f48b0ec3a12789025181436ec044e419532896
Milos Havlicek <miloshavlicek@gmail.com>
2016-07-21 12:00:47 +0200

    fixes for nette 2.4

--- src/model/LogActivityRepository.php
+++ src/model/LogActivityRepository.php
@@ -4,38 +4,38 @@ namespace App\Cothema\Admin;
 
 /**
  * 
- * @author     Miloš Havlíček <miloshavlicek@gmail.com>
+ * @author Miloš Havlíček <miloshavlicek@gmail.com>
  */
 class LogActivityRepository {
 
-	public static function logActivity($em, $userId) {
-		$activity = self::getActivityIfExists($em, $userId);
+    public static function logActivity($em, $userId) {
+        $activity = self::getActivityIfExists($em, $userId);
 
-		if (isset($activity[0])) {
-			$activity[0]->timeTo = new \DateTime();
-			$em->persist($activity[0]);
-		} else {
-			$newActivity = new LogActivity;
-			$newActivity->user = $userId;
-			$newActivity->timeFrom = new \DateTime();
-			$newActivity->timeTo = new \DateTime();
-			$em->persist($newActivity);
-		}
+        if (isset($activity[0])) {
+            $activity[0]->timeTo = new \DateTime();
+            $em->persist($activity[0]);
+        } else {
+            $newActivity = new LogActivity;
+            $newActivity->user = $userId;
+            $newActivity->timeFrom = new \DateTime();
+            $newActivity->timeTo = new \DateTime();
+            $em->persist($newActivity);
+        }
 
-		$em->flush();
-	}
+        $em->flush();
+    }
 
-	private static function getActivityIfExists($em, $userId) {
-		$qb = $em->createQueryBuilder('a');
-		$qb->select('a');
-		$qb->from(LogActivity::class, 'a');
-		$qb->where('a.user = :user AND a.timeTo >= :timeTo');
-		$qb->orderBy('a.id', 'DESC');
-		$qb->setMaxResults(1);
-		$qb->setParameter('user', $userId);
-		$qb->setParameter('timeTo', new \DateTime('-10 minutes'));
+    private static function getActivityIfExists($em, $userId) {
+        $qb = $em->createQueryBuilder();
+        $qb->select('a');
+        $qb->from(LogActivity::class, 'a');
+        $qb->where('a.user = :user AND a.timeTo >= :timeTo');
+        $qb->orderBy('a.id', 'DESC');
+        $qb->setMaxResults(1);
+        $qb->setParameter('user', $userId);
+        $qb->setParameter('timeTo', new \DateTime('-10 minutes'));
 
-		return $qb->getQuery()->getResult();
-	}
+        return $qb->getQuery()->getResult();
+    }
 
 }
