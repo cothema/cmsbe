076f3b4bb2ff2c561bc2510c4b9e11b2b635a78d
Milos Havlicek <miloshavlicek@gmail.com>
2017-02-07 12:25:15 +0100

    fix: ROUTE::SECURED is deprecated

--- src/router/RouterFactory.php
+++ src/router/RouterFactory.php
@@ -8,18 +8,21 @@ use Nette\Application\Routers\Route;
 /**
  * Router factory.
  */
-class RouterFactory {
+class RouterFactory
+{
 
     /**
      * @return \Nette\Application\IRouter
      */
-    public function createRouter() {
+    public function createRouter()
+    {
         !defined('SECURED') && define('SECURED', FALSE);
-        $sslForce = (SECURED && !LOCALHOST && !DEV_MODE) ? Route::SECURED : FALSE;
+        $sslForce = (SECURED && !LOCALHOST && !DEV_MODE);
+        $mask     = $sslForce ? 'https://' : '//';
 
-        $router = new RouteList();
-        $router[] = new Route('<locale=cz cz|en|nl>/<presenter>/<action>[/<id>]', 'Homepage:default', $sslForce);
+        $router   = new RouteList();
+        $router[] = new Route($mask.'<locale=cz cz|en|nl>/<presenter>/<action>[/<id>]',
+            'Homepage:default');
         return $router;
     }
-
 }
