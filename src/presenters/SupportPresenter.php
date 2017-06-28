540f6b5c16396c50e880f6306a050d4c2c16dc94
Milos Havlicek <miloshavlicek@gmail.com>
2017-06-28 09:13:49 +0200

    formatting

--- src/presenters/SupportPresenter.php
+++ src/presenters/SupportPresenter.php
@@ -10,28 +10,30 @@ namespace App\Presenters;
  *
  * Support Presenter
  */
-final class SupportPresenter extends BasePresenter {
+final class SupportPresenter extends BasePresenter
+{
 
-	public function renderDefault() {
-		$navbar = [];
-		$navbar[] = (object) ['name' => 'Pomoc', 'link' => 'Help:'];
-		$navbar[] = (object) ['name' => 'Podpora systému'];
+    public function renderDefault()
+    {
+        $navbar   = [];
+        $navbar[] = (object) ['name' => 'Pomoc', 'link' => 'Help:'];
+        $navbar[] = (object) ['name' => 'Podpora systému'];
 
-		$this->template->navbar = $navbar;
+        $this->template->navbar = $navbar;
 
-		$this->template->contact = $this->getSupportContact();
-	}
+        $this->template->contact = $this->getSupportContact();
+    }
 
-	/**
-	 *
-	 * @return object
-	 */
-	private function getSupportContact() {
-		return (object) [
-					'name' => 'Miloš Havlíček',
-					'tel' => '+420 606 460 316',
-					'email' => 'miloshavlicek@gmail.com'
-		];
-	}
-
+    /**
+     *
+     * @return object
+     */
+    private function getSupportContact()
+    {
+        return (object) [
+                'name' => 'Miloš Havlíček',
+                'tel' => '+420 606 460 316',
+                'email' => 'miloshavlicek@gmail.com'
+        ];
+    }
 }
